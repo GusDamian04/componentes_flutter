33324263ac8c429104f6ea614b43214d08b50f55
GusDamian <damiangustavoangel@gmail.com>
2024-02-14 12:53:27 -0600

    Adding the ListView into the Scaffold

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:practica3/home_screen.dart';
+import 'home_screen.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -8,6 +8,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
